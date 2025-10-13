459000c7ac2fa0fb805acce2c4ba8308081d1015
abdellatif <abdellatif.errami@gmail.com>
2025-10-13 23:36:21 +0200

    Add c08 ex02

---
+++ c08/ex02/ft_abs.h
@@ -0,0 +1,4 @@
+#ifndef FT_ABS
+#define FT_ABS
+#define ABS(Value) (((Value) < 0) ? ((Value) * (-1)) : (Value))
+#endif
---
+++ c08/ex02/main.c
@@ -0,0 +1,7 @@
+#include "ft_abs.h"
+#include <stdio.h>
+int main(void)
+{
+	int i = ABS(-10);
+	printf("%i", i);
+}
