e188675b9c1d5c949a360ad4faae3b51c9221d12
abdellatif <abdellatif.errami@gmail.com>
2025-10-13 23:00:18 +0200

    Add c08 ex01

---
+++ c08/ex01/ft_boolean.h
@@ -0,0 +1,16 @@
+#ifndef FT_BOOLEAN_H
+# define FT_BOOLEAN_H
+
+# include <unistd.h>
+
+# define EVEN(n) (((n) % 2) == 0)
+# define TRUE 1
+# define FALSE 0
+# define SUCCESS TRUE
+# define EVEN_MSG "I have an even number of arguments.\n"
+# define ODD_MSG "I have an odd number of arguments.\n"
+
+typedef int	t_bool;
+
+#endif
+
